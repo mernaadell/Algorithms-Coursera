7bea74b43de934b3a36c24dacf5cc3befb3ba9b0
mernaadell <mernaadel963@gmail.com>
2019-12-11 20:17:03 +0200

    week1 solution

---
+++ Algorithmic Toolbox Course1/week1_programming_challenges/sum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 19:41:25 2019
+
+@author: merna
+"""
+
+
+if __name__ == '__main__':
+   
+    arr = list(map(int, input().rstrip().split()))
+    print(arr[0]+arr[1])
+    
+  
